43d500c08920fdd390bc40267611d313208f81cf
Scott McPeak <scott.g.mcpeak@gmail.com>
2017-11-26 13:58:32 -0800

    Keep track of all vehicles.

--- control.lua
+++ control.lua
@@ -39,9 +39,76 @@ script.on_load(function()
   log("VehicleLeash on_load called.");
 end);
 
+-- True once we have scanned the world for vehicles after loading.
+local found_vehicles = false;
+
+-- Map from force to its vehicles.  Each force's vehicles are a map
+-- from unit_number to the vehicle entity.
+local force_to_vehicles = {};
+
+-- Add a vehicle to our table.
+local function add_vehicle(v)
+  local force_name = string_or_name_of(v.force);
+  force_to_vehicles[force_name] = force_to_vehicles[force_name] or {};
+  force_to_vehicles[force_name][v.unit_number] = v;
+  log("Vehicle " .. v.unit_number ..
+      " with name " .. v.name ..
+      " at (" .. v.position.x .. "," .. v.position.y .. ")" ..
+      " added to force " .. force_name);
+end;
+
+-- Scan the world for vehicles.
+local function find_vehicles()
+  log("VehicleLeash: find_vehicles");
+
+  local vehicles = game.surfaces[1].find_entities_filtered{
+    type = "car",
+  };
+  for _, v in ipairs(vehicles) do
+    --log("found vehicle: " .. serpent.block(entity_info(v)));
+    add_vehicle(v);
+  end;
+end;
+
+local function remove_invalid_vehicles()
+  for force, vehicles in pairs(force_to_vehicles) do
+    local num_vehicles = 0;
+    for unit_number, v in pairs(vehicles) do
+      if (v.valid) then
+        num_vehicles = num_vehicles + 1;
+      else
+        vehicles[unit_number] = nil;
+        log("Removed invalid vehicle " .. unit_number .. ".");
+      end;
+    end;
+    log("Force " .. force .. " has " .. num_vehicles .. " vehicles.");
+  end;
+end;
+
 script.on_event(defines.events.on_tick, function(e)
+  if (not found_vehicles) then
+    found_vehicles = true;
+    find_vehicles();
+  end;
+
   if not ((e.tick % 60) == 0) then return; end;
   log("VehicleLeash once per second event called.");
+
+  remove_invalid_vehicles();
+
+
 end);
+
+-- On built entity: add to tables.
+script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity},
+  function(e)
+    local ent = e.created_entity;
+    --log("VehicleLeash: saw built event: " .. serpent.block(entity_info(ent)));
+    if (ent.type == "car") then
+      add_vehicle(ent);
+    end;
+  end
+);
+
 
 -- EOF
