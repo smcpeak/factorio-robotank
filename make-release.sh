2e7c669b7877514fd3dbd4f0796489c2dc70a6fa
Scott McPeak <scott.g.mcpeak@gmail.com>
2017-12-04 22:55:49 -0800

    Add make-release.sh.

---
+++ make-release.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Make a release image
+
+if [ "x$1" = "x" ]; then
+  echo "usage: $0 RoboTank_X.Y.Z"
+  echo "Run this in the parent directory of the RoboTank directory."
+  exit 2
+fi
+
+directory_name="$1"
+if echo "$directory_name" | grep '/'; then
+  echo "Directory name must not contain a slash character."
+  exit 2
+fi
+if [ ! -d "$directory_name" ]; then
+  echo "Directory $directory_name not found."
+  exit 2
+fi
+
+zipfile="$directory_name.zip"
+if [ -f "$zipfile" ]; then
+  echo "rm $zipfile"
+  rm "$zipfile" || exit
+fi
+
+echo zip -r "$zipfile" "$directory_name" -x '*.git*'
+exec zip -r "$zipfile" "$directory_name" -x '*.git*'
+
+# EOF
